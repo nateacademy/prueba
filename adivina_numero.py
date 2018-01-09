72aabede4ca014f41eff0213354d2ea84dc81426
Nate Gentile <techshowes@gmail.com>
2018-01-09 18:13:34 +0100

    Prueba de variables

---
+++ adivina_numero.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+# Tipos
+"Hola"          # String, cadena de texto
+3               # Number, numero
+True / False    # Boolean
+
+# Asignación - reglas
+numero = 34
+texto = "Hola"
+verdadero = True
+
+# Operaciones
+3 + 4
+"Hola " + "Mundo"
+4 - 2
+2 * 4
+6 / 3
+
+# Operaciones booleanas - devuelven verdadero o falso
+2 == 2
+3 == 2
+2 < 1
+3 > 1
+3 >= 3
+4 >= 20
+
+# Funciones - parametros y return
+print("Adivina que número estoy pensando")
+input("Dime un número")
+max(2, 3, 4, 3, 1)
+min(2, 3, 4, 1)
+
+# Estructuras de control
+if 3 == 3:
+    print("Son iguales")
+else:
+    print("Son distintos")
+
